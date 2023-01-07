224a65dca19c541c96a537d1c2693d4d088a653b
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-01-07 18:38:14 +0100

    style tasks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { formatDistance, subDays } from 'date-fns'
-
-
 const test = formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
 
-
-
 import printMe from './dom.js';
-
 console.log(printMe())
 
+// write the logic here (object creation and editing, eventlisteners)
+
+const newItemButton = document.querySelector(".newItemButton")
+
+newItemButton.addEventListener("click", openModul)
+
+function openModul(){
+    console.log("warnung")
+}
